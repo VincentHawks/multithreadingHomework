a0644ac86474aa076f2fd1f1bb8fb152c75a7923
Yastrebov Vsevolod <vyastrebov@VYASTREBOV-A1.local>
2021-02-17 09:25:39 +0300

    Implement Thread parallelism

--- app/src/main/java/com/swtecnn/sprinkler/MainActivity.kt
+++ app/src/main/java/com/swtecnn/sprinkler/MainActivity.kt
@@ -3,6 +3,7 @@ package com.swtecnn.sprinkler
 import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.*
 import androidx.constraintlayout.widget.Guideline
@@ -10,19 +11,77 @@ import androidx.core.view.children
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.swtecnn.sprinkler.api.RetrofitClient
+import com.swtecnn.sprinkler.api.model.CurrentWeather
+import com.swtecnn.sprinkler.api.model.WeatherForecast
 import com.swtecnn.sprinkler.view.adapters.ForecastAdapter
 import com.swtecnn.sprinkler.view.adapters.LocationAdapter
 import com.swtecnn.sprinkler.view.models.Forecast
 import com.swtecnn.sprinkler.view.models.Location
+import com.swtecnn.sprinkler.view.models.fromDailyForecast
+import kotlin.concurrent.thread
+import kotlin.math.floor
+
+const val ONE_HOUR_MILLIS = 3600000L
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var sprinklerSwitcher: ImageSwitcher
     private lateinit var forecastView: RecyclerView
     private lateinit var locationView: RecyclerView
+    @Volatile private lateinit var tempValue: TextView
+    @Volatile private lateinit var humidValue: TextView
     private var sprinklerOnline = true
 
-    @Volatile var forecasts: List<Forecast> = mutableListOf()
+    private var forecastUpdateThread = Thread { // updates current weather and the forecast every hour
+        while(!Thread.interrupted()) {
+            val weatherForecastResponse = RetrofitClient.getWeatherForecast().execute()
+            val currentWeatherResponse = RetrofitClient.getCurrentWeather().execute()
+
+            if (!weatherForecastResponse.isSuccessful) {
+                Log.println(
+                    Log.ERROR, "WeatherThread",
+                    "Weather forecast couldn't be fetched, reason: ${weatherForecastResponse.errorBody()}"
+                )
+            }
+
+            if (!currentWeatherResponse.isSuccessful) {
+                Log.e(
+                    "WeatherThread",
+                    "Current weather couldn't be fetched, reason: ${currentWeatherResponse.errorBody()}"
+                )
+            }
+
+            if (weatherForecastResponse.isSuccessful && currentWeatherResponse.isSuccessful) {
+                // If fetched successfully, update the data on screen
+                val digestedForecast: MutableList<Forecast> = mutableListOf()
+                val rawForecast: WeatherForecast = weatherForecastResponse.body()!!
+                val rawCurrentWeather: CurrentWeather = currentWeatherResponse.body()!!.weather
+                for(forecast in rawForecast.daily) {
+                    digestedForecast.add(fromDailyForecast(forecast))
+                }
+                if(!Thread.interrupted()) {
+                    runOnUiThread {
+                        (forecastView.adapter!! as ForecastAdapter).forecast.clear()
+                        (forecastView.adapter!! as ForecastAdapter).forecast.addAll(digestedForecast)
+                        forecastView.adapter!!.notifyDataSetChanged()
+                        tempValue.text = "${floor(rawCurrentWeather.temp.toDouble()).toInt()}°"
+                        humidValue.text = "${rawCurrentWeather.humidity}%"
+                    }
+                } else {
+                    return@Thread
+                }
+            }
+
+            try {
+                Thread.sleep(ONE_HOUR_MILLIS)
+            } catch (e: InterruptedException) {
+                return@Thread
+            }
+        }
+    }
+
+    @Volatile var forecasts: MutableList<Forecast> = mutableListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,8 +91,11 @@ class MainActivity : AppCompatActivity() {
         val locationLayoutManager = LinearLayoutManager(this)
         forecastView = findViewById(R.id.forecast)
         locationView = findViewById(R.id.locations)
+        tempValue = findViewById(R.id.tempValue)
+        humidValue = findViewById(R.id.humidValue)
+
         locationView.layoutManager = locationLayoutManager
-        forecastView.setHasFixedSize(true)
+        //forecastView.setHasFixedSize(true)
         locationView.setHasFixedSize(true)
         locationView.addItemDecoration(DividerItemDecoration(locationView.context, locationView.layoutManager!!.layoutDirection))
 
@@ -57,6 +119,7 @@ class MainActivity : AppCompatActivity() {
 
         forecastView.adapter =
             ForecastAdapter(this, forecasts)
+        forecastUpdateThread.start()
 
         val locations = listOf(
             Location("Backyard"),
@@ -89,5 +152,10 @@ class MainActivity : AppCompatActivity() {
                 component.findViewById<CheckBox>(R.id.checkBox).isChecked = false
             }
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        forecastUpdateThread.interrupt()
     }
 }
